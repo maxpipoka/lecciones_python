f53810d8ef5a026797d7d673d46b4aeb5953ce46
maxpipoka <martincardozo@gmail.com>
2021-07-22 19:54:08 -0300

    Can_lease method´s test added. Leccion_02 ended.

--- leccion_02/main.py
+++ leccion_02/main.py
@@ -7,23 +7,29 @@ class VehicleInfo:
             raise ValueError(f"Catalogue price should be a positive number, but received {catalogue_price} instead.")
         self.catalogue_price = catalogue_price
 
-
     # This method computes the tax payable for this particular vehicle and
     # returns that as a positive floating point value.
     # You can optionally provide an amount below which no tax is computed
+
     def compute_tax(self, tax_exemption_amount: int = 0) -> float:
         if tax_exemption_amount < 0:
-            raise ValueError(f"tax_exemption_amount should be a positive number, but received {tax_exemption_amount} instead.")
+            raise ValueError(
+                f"tax_exemption_amount should be a positive number, but received {tax_exemption_amount} instead."
+            )
         tax_percentage = 0.05
         if self.electric:
             tax_percentage = 0.02
-        return tax_percentage * (self.catalogue_price - tax_exemption_amount)
+
+        return tax_percentage * max(self.catalogue_price - tax_exemption_amount, 0)
 
     # you can only lease this car if the catalogue price is not more than 70% of
     # your year income; year_income should be >= 0
     def can_lease(self, year_income: int) -> bool:
-        # to do
-        pass
+        if year_income < 0:
+            raise ValueError(
+                f"Year Income should be a positive number, but received {year_income} instead."
+            )
+        return self.catalogue_price <= year_income * 0.7
 
 # create a vehicle info object
 # v = VehicleInfo("BMW", False, 10000)
--- leccion_02/test/unittest/test_vehicle_info.py
+++ leccion_02/test/unittest/test_vehicle_info.py
@@ -6,7 +6,6 @@ class TestVehicleInfo(unittest.TestCase):
     def test_non_electric_vehicle(self):
         # create a vehicle info object
         v = VehicleInfo("BMW", False, 10000)
-
         # compute the tax
         tax = v.compute_tax()
 
@@ -15,7 +14,6 @@ class TestVehicleInfo(unittest.TestCase):
     def test_electric_vehicle(self):
         # create a vehicle info object
         v = VehicleInfo("BMW", True, 10000)
-
         # compute the tax
         tax = v.compute_tax()
 
@@ -24,7 +22,6 @@ class TestVehicleInfo(unittest.TestCase):
     def test_tax_exemption_amount_non_electric(self):
         # create a vehicle info object
         v = VehicleInfo("BMW", False, 10000)
-
         # compute the tax
         tax = v.compute_tax(5000)
 
@@ -33,7 +30,6 @@ class TestVehicleInfo(unittest.TestCase):
     def test_tax_exemption_amount_electric(self):
         # create a vehicle info object
         v = VehicleInfo("BMW", True, 10000)
-
         # compute the tax
         tax = v.compute_tax(5000)
 
@@ -52,6 +48,34 @@ class TestVehicleInfo(unittest.TestCase):
         self.assertRaises(ValueError, VehicleInfo, "BMW", True, -10000)
         # devuelve verdadero si devuelve error del tipo ValueError, al llamar la funcion pasandole ese valor.
 
+    def test_catalogue_price_minor_exemption_amonunt(self):
+        # create a vehicle info object
+        v = VehicleInfo("BMW", True, 10000)
+        # compute the tax
+        tax = v.compute_tax(15000)
+
+        self.assertEqual(tax, 0)
+
+    # you can only lease this car if the catalogue price is not more than 70% of
+    # your year income; year_income should be >= 0
+    # def can_lease(self, year_income: int) -> bool:
+
+    def test_catalogue_price_minor_70_percent_year_income(self):
+        v = VehicleInfo("BMW", True, 69)
+        self.assertTrue(v.can_lease(100))
+
+    def test_catalogue_price_equal_70_percent_year_income(self):
+        v = VehicleInfo("BMW", True, 70)
+        self.assertTrue(v.can_lease(100))
+
+    def test_catalogue_price_mayor_70_percent_year_income(self):
+        v = VehicleInfo("BMW", True, 71)
+        self.assertFalse(v.can_lease(100))
+
+    def test_year_income_value_negative(self):
+        v = VehicleInfo("BMW", True, 71)
+        self.assertRaises(ValueError, v.can_lease, -1)
+        # devuelve verdadero si devuelve error del tipo ValueError, al llamar la funcion pasandole ese valor.
 
 if __name__ == '__main__':
     unittest.main()
